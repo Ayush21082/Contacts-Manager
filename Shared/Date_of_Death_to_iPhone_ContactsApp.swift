a735a9c326f7453497d1196b34ec6c3866414dea
Ayush Singh <ayush.singh21082@gmail.com>
2022-09-19 23:35:40 +0530

    Initial Commit

---
+++ Shared/Date_of_Death_to_iPhone_ContactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Date_of_Death_to_iPhone_ContactsApp.swift
+//  Shared
+//
+//  Created by Ayush Singh on 19/09/22.
+//
+
+import SwiftUI
+
+@main
+struct Date_of_Death_to_iPhone_ContactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
